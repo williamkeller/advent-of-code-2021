1bff462afefb94e3daab2c16bc161442eb5d36e7
William Keller <william@arda.net>
2021-12-04 11:51:11 -0600

    Day 4

---
+++ day04.rb
@@ -0,0 +1,118 @@
+
+def evaulate_board(b)
+  # Rows
+  for i in 0...5
+    if b[i][0] == '*' &&
+       b[i][1] == '*' &&
+       b[i][2] == '*' &&
+       b[i][3] == '*' &&
+       b[i][4] == '*'
+      return true
+    end
+  end
+
+  # Columns
+  for i in 0...5
+    if b[0][i] == '*' &&
+       b[1][i] == '*' &&
+       b[2][i] == '*' &&
+       b[3][i] == '*' &&
+       b[4][i] == '*'
+      return true
+    end
+  end
+
+  false
+end
+
+
+def mark_board(board, value)
+  for i in 0...5
+    for j in 0...5
+      if board[i][j] == value
+        board[i][j] = '*'
+      end
+    end
+  end
+end
+
+
+def score_board(board)
+  sum = 0
+  for i in 0...5
+    for j in 0...5
+      sum += board[i][j] unless board[i][j] == '*'
+    end
+  end
+
+  sum
+end
+
+def part_one(data)
+  picks = data[0].split(/,/).map(&:to_i)
+  boards = []
+
+  index = 1
+  while true
+    board = []
+    for i in 1..5
+      board << data[index + i].split.map(&:to_i)
+    end
+    boards << board
+    index += 6
+    break if index == data.length
+  end
+
+  picks.each do |pick|
+    boards.each do |board|
+      mark_board(board, pick)
+      if evaulate_board(board) == true
+        sum = score_board(board)
+        puts sum * pick
+        return
+      end
+    end
+  end
+end
+
+def count_active_boards(boards)
+  count = 0
+  boards.each do |board|
+    count += 1 unless board.nil?
+  end
+  count
+end
+
+def part_two(data)
+  picks = data[0].split(/,/).map(&:to_i)
+  boards = []
+
+  index = 1
+  while true
+    board = []
+    for i in 1..5
+      board << data[index + i].split.map(&:to_i)
+    end
+    boards << board
+    index += 6
+    break if index == data.length
+  end
+
+  picks.each do |pick|
+    boards.each_with_index do |board, i|
+      next if board.nil?
+
+      mark_board(board, pick)
+      if evaulate_board(board) == true
+        if count_active_boards(boards) == 1
+          score = score_board(board)
+          puts pick, score, score * pick
+
+          return
+        else
+          boards[i] = nil
+        end
+      end
+    end
+  end
+end
