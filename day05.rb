62ff1c42b513bee62571480f962040d05c65e7c2
William Keller <william@arda.net>
2021-12-05 13:54:22 -0600

    Day 5

--- day05.rb
+++ day05.rb
@@ -1,9 +1,5 @@
-
-
-RE = /(\d+),(\d+) \-> (\d+),(\d+)/
-
 def parse_line(line)
-  match = line.match RE
+  match = line.match /(\d+),(\d+) \-> (\d+),(\d+)/
 
   raise 'Regular expression didn\'t match' if match.nil?
 
@@ -11,76 +7,59 @@ def parse_line(line)
     x2: match[3].to_i, y2: match[4].to_i }
 end
 
+
 def generate_cells(vent)
-  vents = []
+  xrun = nil
+  yrun = nil
 
   # Horizontal run
   if vent[:x1] == vent[:x2]
-    if vent[:y1] < vent[:y2]
-      for i in vent[:y1]..vent[:y2]
-        vents << [vent[:x1], i]
-      end
-    else
-      for i in vent[:y2]..vent[:y1]
-        vents << [vent[:x1], i]
-      end
-    end
-  elsif vent[:y1] == vent[:y2]
-    if vent[:x1] < vent[:x2]
-      for i in vent[:x1]..vent[:x2]
-        vents << [i, vent[:y1]]
-      end
-    else
-      for i in vent[:x2]..vent[:x1]
-        vents << [i, vent[:y1]]
-      end
-    end
-  elsif (vent[:x1] - vent[:x2]).abs == (vent[:y1] - vent[:y2]).abs
-    puts "*** diagonal ***"
-    puts vent.inspect
-    x = (vent[:x1] < vent[:x2]) ? (vent[:x1]..vent[:x2]).to_a :
-      (vent[:x2]..vent[:x1]).to_a.reverse
-    y = (vent[:y1] < vent[:y2]) ? (vent[:y1]..vent[:y2]).to_a :
+    xrun = Array.new((vent[:y1] - vent[:y2]).abs + 1, vent[:x1])
+    yrun = (vent[:y1] < vent[:y2]) ? (vent[:y1]..vent[:y2]).to_a :
       (vent[:y2]..vent[:y1]).to_a.reverse
 
-    vents = x.zip(y)
-    puts vents.inspect
+  # Vertical run
+  elsif vent[:y1] == vent[:y2]
+
+    xrun = (vent[:x1] < vent[:x2]) ? (vent[:x1]..vent[:x2]).to_a :
+      (vent[:x2]..vent[:x1]).to_a.reverse
+    yrun = Array.new((vent[:x1] - vent[:x2]).abs + 1, vent[:y1])
+
+  # Diagonal run
+  elsif (vent[:x1] - vent[:x2]).abs == (vent[:y1] - vent[:y2]).abs
+    xrun = (vent[:x1] < vent[:x2]) ? (vent[:x1]..vent[:x2]).to_a :
+      (vent[:x2]..vent[:x1]).to_a.reverse
+    yrun = (vent[:y1] < vent[:y2]) ? (vent[:y1]..vent[:y2]).to_a :
+      (vent[:y2]..vent[:y1]).to_a.reverse
   end
 
-  vents
+  xrun.zip(yrun)
 end
+
+
+def count_intersections(cells)
+  counts = Hash.new { 0 }
+  cells.each do |cell|
+    counts[cell] += 1
+  end
+
+  total = counts.values.count { |cell| cell > 1 }
+  puts total
+end
+
 
 def part_one(data)
   vents  = data.map { |d| parse_line d }
     .filter { |d| d[:x1] == d[:x2] || d[:y1] == d[:y2] }
 
-  cells = []
-  vents.each do |vent|
-    cells.concat(generate_cells(vent))
-  end
-
-  counts = Hash.new { 0 }
-  cells.each do |cell|
-    counts[cell] += 1
-  end
-
-  total = counts.values.count { |cell| cell > 1 }
-  puts total
+  cells = vents.map { |vent| generate_cells(vent) }.flatten(1)
+  count_intersections cells
 end
+
 
 def part_two(data)
   vents  = data.map { |d| parse_line d }
 
-  cells = []
-  vents.each do |vent|
-    cells.concat(generate_cells(vent))
-  end
-
-  counts = Hash.new { 0 }
-  cells.each do |cell|
-    counts[cell] += 1
-  end
-
-  total = counts.values.count { |cell| cell > 1 }
-  puts total
+  cells = vents.map { |vent| generate_cells(vent) }.flatten(1)
+  count_intersections cells
 end
