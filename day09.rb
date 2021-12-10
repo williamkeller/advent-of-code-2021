86d12272a9ba2aab7fa5e7b96be051e912aa4b8f
William Keller <william@arda.net>
2021-12-09 21:11:01 -0600

    Day 9

--- day09.rb
+++ day09.rb
@@ -1,53 +1,54 @@
 def transform_data(data)
-  data.map do |row|
-    row.chars.map &:to_i
-  end
-end
+  cells = Hash.new { 9 }
 
-
-def surrounding(map, x, y)
-  xmax = map[0].length - 1
-  ymax = map.length - 1
-
-  # puts "#{x}, #{y}"
-  cur_val = map[y][x]
-  neighbors = []
-
-  neighbors << map[y][x - 1] if x > 0
-  neighbors << map[y][x + 1] if x < xmax
-
-  neighbors << map[y - 1][x] if y > 0
-  neighbors << map[y + 1][x] if y < ymax
-
-
-
-  # neighbors << map[y][x - 1] if x > 0 && y >= 0
-  # neighbors << map[y - 1][x] if x >= 0 && y > 0
-
-  # neighbors << map[y][x + 1] if x < (map[0].length - 2)
-  # neighbors << map[y + 1][x] if y < (map.length - 2)
-
-  # puts "#{cur_val}   #{neighbors.inspect}"
-  neighbors.find_index { |n| n <= cur_val }.nil?
-end
-
-
-def part_one(data)
-  xmax = data[0].length
-  ymax = data.length
-
-  total = 0
-
-  # puts data.inspect
-  (0...xmax).each do |x|
-    (0...ymax).each do |y|
-      if surrounding(data, x, y)
-        total += (data[y][x] + 1)
-      end
+  data.each_with_index do |line, r|
+    values = line.chars.map(&:to_i)
+    values.each_with_index do |cell, c|
+      cells[[r, c]] = cell
     end
   end
 
-  puts
-  puts total
+  cells
+end
 
+
+def low_points(cells)
+  cells.filter do |key, cell|
+    x, y = key
+
+    cells[[x - 1, y]] > cell &&
+      cells[[x + 1, y]] > cell &&
+      cells[[x, y - 1]] > cell &&
+      cells[[x, y + 1]] > cell
+  end
+end
+
+
+def spread(cells, x, y)
+  cells[[x, y]] = 10 # mark the current cell counted
+  count = 1
+
+  neighbors = [[x, y - 1], [x - 1, y], [x, y + 1], [x + 1, y]]
+
+  neighbors.each do |coords|
+    if cells[coords] < 9
+      count += spread(cells, coords[0], coords[1])
+    end
+  end
+
+  count
+end
+
+
+def part_one(cells)
+  total = low_points(cells).values.sum { |c| c + 1 }
+  puts total
+end
+
+
+def part_two(cells)
+  points = low_points(cells).keys
+  sizes = points.map { |p| spread(cells, p[0], p[1]) }
+  total = sizes.sort { |a, b| b <=> a }.first(3).reduce(1) { |v, accum| v * accum }
+  puts total
 end
